7451c62195d4ed05202f6677643570c0725e299c
PC-20230725BPBH\Administrator <MrYuanJw@163.com>
2023-08-31 02:25:05 +0800

    测试

--- src/comment/gmme/index.tsx
+++ src/comment/gmme/index.tsx
@@ -1,5 +1,5 @@
+import { log } from "console";
 import React from "react";
-
 
 export default function index() {
   var arr = [1, 2, "hello", 34, 2, 5, 34, "hello", 2];
@@ -14,6 +14,9 @@ export default function index() {
       return array.indexOf(item) === index;
     });
   };
+  console.log('====================================');
+  console.log(111111111111);
+  console.log('====================================');
   //   const unique2 = (arr: any[], key: string | number) => {
   //     const res = new Map();
   //     return arr.filter((item) => !res.has(item[key]) && res.set(item[key], 1));
